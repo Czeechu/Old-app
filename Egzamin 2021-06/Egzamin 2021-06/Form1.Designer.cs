e45894b31291dca10ef23624fad98071b4780e14
Czeechu <112950394+Czeechu@users.noreply.github.com>
2024-04-08 17:43:42 +0200

    Add files via upload

---
+++ Egzamin 2021-06/Egzamin 2021-06/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Egzamin_2021_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTittle = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.labelPass = new System.Windows.Forms.Label();
+            this.textBoxHaslo = new System.Windows.Forms.TextBox();
+            this.labelPass2 = new System.Windows.Forms.Label();
+            this.buttonAccept = new System.Windows.Forms.Button();
+            this.textBoxPowtorzoneHaslo = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTittle
+            // 
+            this.labelTittle.AutoSize = true;
+            this.labelTittle.BackColor = System.Drawing.Color.Teal;
+            this.labelTittle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTittle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.labelTittle.Location = new System.Drawing.Point(16, 20);
+            this.labelTittle.Name = "labelTittle";
+            this.labelTittle.Size = new System.Drawing.Size(156, 25);
+            this.labelTittle.TabIndex = 0;
+            this.labelTittle.Text = "Rejestruj konto";
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEmail.Location = new System.Drawing.Point(14, 62);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(86, 16);
+            this.labelEmail.TabIndex = 1;
+            this.labelEmail.Text = "Podaj e-mail:";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(17, 81);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(151, 20);
+            this.textBoxEmail.TabIndex = 2;
+            // 
+            // labelPass
+            // 
+            this.labelPass.AutoSize = true;
+            this.labelPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPass.Location = new System.Drawing.Point(14, 116);
+            this.labelPass.Name = "labelPass";
+            this.labelPass.Size = new System.Drawing.Size(82, 16);
+            this.labelPass.TabIndex = 3;
+            this.labelPass.Text = "Podaj haslo:";
+            // 
+            // textBoxHaslo
+            // 
+            this.textBoxHaslo.Location = new System.Drawing.Point(17, 135);
+            this.textBoxHaslo.Name = "textBoxHaslo";
+            this.textBoxHaslo.Size = new System.Drawing.Size(151, 20);
+            this.textBoxHaslo.TabIndex = 4;
+            this.textBoxHaslo.UseSystemPasswordChar = true;
+            // 
+            // labelPass2
+            // 
+            this.labelPass2.AutoSize = true;
+            this.labelPass2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPass2.Location = new System.Drawing.Point(18, 171);
+            this.labelPass2.Name = "labelPass2";
+            this.labelPass2.Size = new System.Drawing.Size(93, 16);
+            this.labelPass2.TabIndex = 5;
+            this.labelPass2.Text = "Powtorz haslo:";
+            // 
+            // buttonAccept
+            // 
+            this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAccept.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.buttonAccept.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonAccept.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonAccept.Location = new System.Drawing.Point(79, 274);
+            this.buttonAccept.Name = "buttonAccept";
+            this.buttonAccept.Size = new System.Drawing.Size(113, 57);
+            this.buttonAccept.TabIndex = 7;
+            this.buttonAccept.Text = "ZATWIERDŹ";
+            this.buttonAccept.UseVisualStyleBackColor = false;
+            this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+            // 
+            // textBoxPowtorzoneHaslo
+            // 
+            this.textBoxPowtorzoneHaslo.Location = new System.Drawing.Point(21, 190);
+            this.textBoxPowtorzoneHaslo.Name = "textBoxPowtorzoneHaslo";
+            this.textBoxPowtorzoneHaslo.Size = new System.Drawing.Size(147, 20);
+            this.textBoxPowtorzoneHaslo.TabIndex = 8;
+            this.textBoxPowtorzoneHaslo.UseSystemPasswordChar = true;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label.Location = new System.Drawing.Point(96, 344);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 16);
+            this.label.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(292, 612);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBoxPowtorzoneHaslo);
+            this.Controls.Add(this.buttonAccept);
+            this.Controls.Add(this.labelPass2);
+            this.Controls.Add(this.textBoxHaslo);
+            this.Controls.Add(this.labelPass);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelTittle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTittle;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelPass;
+        private System.Windows.Forms.TextBox textBoxHaslo;
+        private System.Windows.Forms.Label labelPass2;
+        private System.Windows.Forms.Button buttonAccept;
+        private System.Windows.Forms.TextBox textBoxPowtorzoneHaslo;
+        private System.Windows.Forms.Label label;
+    }
+}
+
